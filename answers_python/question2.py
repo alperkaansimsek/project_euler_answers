d7be09e87dbf756d2bc1dfe3b923cb9381861f49
Alper Kaan Şimşek <47521893+alperkaansimsek@users.noreply.github.com>
2023-01-12 17:51:35 +0300

    Update question2.py

--- answers_python/question2.py
+++ answers_python/question2.py
@@ -1,1 +1,16 @@
+preNum = 0
+counter = 1
+num = 1
+sum = 0
 
+
+while(counter <= 32):
+    fib = preNum + num
+    preNum = num
+    num = fib
+    if(fib % 2 == 0):
+        sum = sum + fib
+    counter = counter + 1
+
+
+print(sum)
