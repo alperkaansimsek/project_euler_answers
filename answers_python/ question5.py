dbfd3f0501617ccfea78ea96cc484fce046c9b90
Alper Kaan Şimşek <47521893+alperkaansimsek@users.noreply.github.com>
2024-05-01 15:50:53 +0300

    Update  question5.py

--- answers_python/ question5.py
+++ answers_python/ question5.py
@@ -1,1 +1,23 @@
-#trying to code it better. When it's completed, I will push it here
+def division (n, number):
+    while n <= 20:
+        if number % n != 0:
+            return False
+        n += 1
+    print(number)
+    return True
+
+
+
+startingPoint = 40
+n = 1
+loopHolder = True
+while loopHolder == True:
+    smallestNumber = division(n, startingPoint)
+    if smallestNumber == True:
+        print(smallestNumber)
+        loopHolder = False
+    else:
+        startingPoint += 1
+        continue
+
+
